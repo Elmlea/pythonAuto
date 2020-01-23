886ac8d11270cb09e845068945d34a5a91eb62cf
Mark Wharry <markwharry@Marks-iMac.local>
2020-01-23 14:07:14 +0000

    Ch2 Q13 finished

--- ch2_q13.py
+++ ch2_q13.py
@@ -1,5 +1,8 @@
-type = "" # initialise the variable because it gets checked in line 2?
-while not "for" or "while" in type:
+type = "" # initialise the variable because it gets checked shortly
+
+print("That's the variable initialised.") # so I know the program has started!
+
+while ("for" or "while") not in type:
     type = input("Would you like to print your numbers using a FOR loop, or a WHILE loop? :>  ")
 
     if "for" in type:
@@ -7,13 +10,17 @@ while not "for" or "while" in type:
         print("Here's a list of numbers!")
         for num in range (1,11):
             print(num)
-            print("These numbers were brought to you using a for loop.")
+        print("These numbers were brought to you using a for loop.")
 
     elif "while" in type:
-        # sing a while loop
+        # using a while loop
         print("Have some numbers!")
         num = 1
         while num < 11:
             print(num)
             num += 1
         print("These ones were made using a while loop.")
+
+    else:
+        print("Be nice if you specified a loop type pls?")
+        #as FOR or WHILE weren't entered, this will restart the external WHILE loop

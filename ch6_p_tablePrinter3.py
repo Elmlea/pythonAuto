fbc3ceb78d7e66aa9e8f69243458123ed6366349
Elmlea <mark.wharry@gmail.com>
2020-01-31 22:13:51 +0000

    Final version of project

---
+++ ch6_p_tablePrinter3.py
@@ -0,0 +1,37 @@
+#! python3
+# Take a list of lists, and print them out nicely!
+
+myTable = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose'],
+             ['please', 'help', 'me', 'Reddit!']]
+tableData = myTable # until it's changed to a function
+
+colWidths = [0] * len(tableData)
+colWidthsIndex = 0
+
+for innerList in tableData:
+    print("The list in use is: " + str(innerList))
+    print("This equates to colWidths index " + str(colWidthsIndex))
+
+    for element in innerList:
+        print("Working on " + element)
+        print("Checking the length of '" + element +"'")
+        print("'" + element + "' is " + str(len(element)) + " characters long.\n")
+
+        if len(element) > colWidths[colWidthsIndex]:
+            colWidths[colWidthsIndex] = len(element)
+            print("Updated colWidths: ", end='')
+            print(colWidths)
+
+    colWidthsIndex += 1
+
+print()
+print("Final Output".center((sum(colWidths) + len(colWidths)), '*'))
+
+for y in range(len(tableData[0])):
+    for x in range(len(tableData)):
+        print(tableData[x][y].rjust(colWidths[x]), end = ' ')
+    print()
+
+print("*" * (sum(colWidths) + len(colWidths)))
